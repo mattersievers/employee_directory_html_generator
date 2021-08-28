0e0d89e03a6281f4935b2e3cde4c101b23921fbe
Matthew Sievers <mattersievers@gmail.com>
2021-08-28 02:08:35 -0500

    adding more members is functional

--- index.js
+++ index.js
@@ -1,29 +1,71 @@
 //include packages
 const inquirer = require('inquirer');
-const Employee = require('./lib/Employee')
+const { choices } = require('yargs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
-function Generator(){
-    Generator.prototype.promptTeamManager = function(){
-        inquirer
-            .prompt({
-                type:'text',
-                name: 'name',
-                message:"What is the team manager's name?"
-            })
-            .then(({name}) =>{
-                this.manager = new Manager(name);
-            })
-        }
+class Directory{
+    constructor(){
 
     }
 
-    const promptTeamIntern = () => {
+    promptTeamManager() {
+       inquirer
+            .prompt([
+                {
+                    type:'text',
+                    name: 'name',
+                    message:"What is the team manager's name?"
+                },
+                {
+                    type:'text',
+                    name: 'id',
+                    message:"What is the team manager's id?"
+                },
+                {
+                    type:'text',
+                    name: 'email',
+                    message:"What is the team manager's email?"
+                },
+                {
+                    type:'text',
+                    name: 'officeNumber',
+                    message:"What is the team manager's office number?"
+                },
+                {
+                    type: 'list',
+                    name:'moreEmployee',
+                    message: 'Would you like to add another person?',
+                    choices: ['add Engineer','add Intern','or Complete the Team']
+                }
+            ])   
+            .then(({name,id,email,officeNumber,moreEmployee}) =>{
+                this.manager = new Manager(name,id,email,officeNumber);
 
+                switch(moreEmployee) {
+                    case 'add Engineer':
+                        return this.promptEngineer();
+                    case 'add Intern':
+                        return this.promptIntern();
+                    case 'or Complete the Team':
+                        return;
+                    default:
+                        return console.log('whoops');
+                }
+            })            
+
+    }   
+    
+    promptEngineer(){
+        console.log('Engineer prompts');
+
+    }
+
+    promptIntern(){
+        console.log('Intern prompts');
+
+    }
 }
 
-const promptTeamEngineer= () =>{}
-
-new Generator().promptTeamManager();
+new Directory().promptTeamManager();
