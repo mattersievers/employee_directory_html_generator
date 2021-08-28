efef4bf2a1761e3fa8a7bb02d8f594712c2725d3
Matthew Sievers <mattersievers@gmail.com>
2021-08-27 21:44:35 -0500

    initial save

---
+++ index.js
@@ -0,0 +1,14 @@
+//include packages
+const inquirer = require('inquirer');
+const page_template = require('./src/page-template.js');
+
+const promptTeamManager = () =>{
+
+}
+
+const promptTeamIntern = () => {
+
+}
+
+promptTeamManager()
+.then()
